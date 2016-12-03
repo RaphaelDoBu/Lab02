3160c4339738df4d700e5ad8f69c3beec894f7bc
Jose Raphael <jose.bu@ccc.ufcg.edu.br>
2016-12-03 19:44:21 -0300

    modificacao

--- Lab02/app.js
+++
@@ -1,8 +0,0 @@
-(function(){
-
-angular.module('appsimples', [])
-.controller('HomeController', function($scope) {
-    $scope.cidades = ['Araguaína', 'Gurupi', 'Palmas', 'Paraíso', 'Porto Nacional'];
-});
-
-})();
